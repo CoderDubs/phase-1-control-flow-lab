e9ca8bd11f73550a6d56b4b39c18ddb5974b54f7
+ <+>
2024-01-09 13:28:50 -0800

    if else tenary switch break default

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(depth){
   // Write your code here!
+  if(depth<=400){
+    return ('This one is on me!');
+  } else if (depth>399 && depth<=2000){
+    return ('That will be twenty bucks.');
+  } else if (depth>2000 && depth<=2500){
+    return ('I will gladly take your thirty bucks.');
+  } else {
+    return ('No can do.');
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipStatus){
   // Write your code here!
+  switch (tipStatus) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.'
+  }
 }
